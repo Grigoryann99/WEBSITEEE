3cc329dd18a4cb31a4362e8b09d1856efdb02d48
Grigoryann99 <yurag2799@gmail.com>
2026-03-10 17:09:45 +0400

    fix(config): allow plus.unsplash.com images in next/image to resolve correctly

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ const nextConfig = {
     images: {
         remotePatterns: [
             { protocol: 'https', hostname: 'images.unsplash.com' },
+            { protocol: 'https', hostname: 'plus.unsplash.com' },
             { protocol: 'https', hostname: 'source.unsplash.com' },
             { protocol: 'https', hostname: 'upload.wikimedia.org' },
             { protocol: 'https', hostname: 'images.pexels.com' },
